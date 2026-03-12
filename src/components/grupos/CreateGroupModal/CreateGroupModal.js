db36b77f5a04e2d035d8b44914ecf8d2d8c29cbe
unknown <patrickgsiqueira@hotmail.com>
2026-03-12 04:40:14 -0300

    Fix: missing useEffect in CreateGroupModal.js

--- src/components/grupos/CreateGroupModal/CreateGroupModal.js
+++ src/components/grupos/CreateGroupModal/CreateGroupModal.js
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Modal from '@/components/ui/Modal/Modal';
 import Input from '@/components/ui/Input/Input';
 import Button from '@/components/ui/Button/Button';
